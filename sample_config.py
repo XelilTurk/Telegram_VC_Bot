3b41b6dd87c06de3eedf86d88d37d792561a0a8d
XelilTurk <70608599+XelilTurk@users.noreply.github.com>
2021-02-16 17:36:30 +0330

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1610939390:AAEI2dNsC3EyfstZ9raq7nJBLzxRKvTYaqA"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485393652
-owner_id = 1243703097
+sudo_chat_id = -1001432160002
+owner_id = 965221088
