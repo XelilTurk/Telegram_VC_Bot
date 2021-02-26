0bd98392710bd31ffbd647d1514127bde7de75cc
iLqar_TurkSoy <70608599+XelilTurk@users.noreply.github.com>
2021-02-27 01:49:19 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-bot_token = "1610939390:AAEI2dNsC3EyfstZ9raq7nJBLzxRKvTYaqA"
+bot_token = "1616365296:AAHjok3iI1l1nyGxVidSF1jb8Th9thGEuQ8"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
 sudo_chat_id = -1001432160002
 owner_id = 965221088
